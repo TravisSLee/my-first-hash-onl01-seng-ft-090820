3185e3fa9d284ce3957f2de7f8f71fdb68605f91
Travis Lee <travisslee223@gmail.com>
2020-09-13 23:22:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -45,3 +45,13 @@ end
 def retrieval
   shipping_manifest["oil paintings"]
 end
+
+def adding
+  shipping_manifest ={
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3,
+                  "muskets" => 2,
+                  "gun powder" = 4
+                 }
+end
